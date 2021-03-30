8b9eaa2659b5c8bbabf484c7d9816f93bf3fd271
Geomasty <elias.sharro@elev.ga.ntig.se>
2021-03-30 16:23:44 +0200

    .

--- Indiv.js
+++ Indiv.js
@@ -5,3 +5,12 @@
 	  function showDiv2() {
 	      document.getElementById('Div2').style.display = "block";
 	  }
+
+	  $(document).ready(function() {
+	      $('select').change(function() {
+	          if ($('select option[value="' + $(this).val() + '"]:selected').length > 1) {
+	              $(this).val('-1').change();
+	              alert('You have already selected this option previously - please choose another.')
+	          }
+	      });
+	  });
