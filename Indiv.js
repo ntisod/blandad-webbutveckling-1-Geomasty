95981b2729666927accbc8374e8616136b092107
Geomasty <elias.sharro@elev.ga.ntig.se>
2021-03-31 13:05:39 +0200

    ,

--- Indiv.js
+++ Indiv.js
@@ -1,16 +1,18 @@
-	  function showDiv() {
-	      document.getElementById('Div1').style.display = "block";
-	  }
+$(document).ready(function() {
+    $('select').change(function() {
+        if ($('select option[value="' + $(this).val() + '"]:selected').length > 1) {
+            $(this).val('-1').change();
+            alert('You have already selected this option previously - please choose another.')
+        }
+    });
+});
 
-	  function showDiv2() {
-	      document.getElementById('Div2').style.display = "block";
-	  }
 
-	  $(document).ready(function() {
-	      $('select').change(function() {
-	          if ($('select option[value="' + $(this).val() + '"]:selected').length > 1) {
-	              $(this).val('-1').change();
-	              alert('You have already selected this option previously - please choose another.')
-	          }
-	      });
-	  });
+$(document).ready(function() {
+    $("div.answer_list").hide();
+    $("input[name$='answer']").click(function() {
+        var test = $(this).val();
+        $("div.answer_list").hide();
+        $("#" + test).show();
+    });
+});
